86c1182c98f022976dffc6a96bcf7ca7cb5bf75a
keerthan2102 <112113302+keerthan2102@users.noreply.github.com>
2025-11-01 11:46:39 -0400

    Update authRoutes.js

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -1,20 +1,31 @@
 const express = require('express');
 const router = express.Router();
+
 const authController = require('../controllers/authController');
+const businessController = require('../controllers/businessController');
 const punchesController = require('../controllers/punchesController');
 const storeController = require('../controllers/storeController');
 const routeController = require('../controllers/routeController');
-const visitController = require('../controllers/visitController');
-const routeStartController = require('../controllers/routeStartController');
-const businessController = require('../controllers/businessController');
+// const visitController = require('../controllers/visitController');
+// const routeStartController = require('../controllers/routeStartController');
 
+// ------- user auth -------
 router.post('/login', authController.login);
 router.post('/signup', authController.signup);
-router.post('/business/login', businessController.businessLogin);
-router.post('/business/signup', businessController.businessSignup);
 
-router.post('/punch', punchesController.punch);
+// ------- business auth -------
+router.post('/business/login', businessController.login);
+router.post('/business/signup', businessController.signup);
 
+const requireAdmin = (req, res, next) => {
+  if (req.headers['x-admin-token'] !== process.env.ADMIN_TOKEN) {
+    return res.status(403).json({ message: 'Unauthorized' });
+  }
+  next();
+};
+router.post('/business/:id/approve', requireAdmin, businessController.approve);
+
+// router.post('/punch', punchesController.punch);
 router.get('/stores/nearby', storeController.getNearbyStores);
 router.post('/stores', storeController.newStore);
 router.get('/stores/:id/name', storeController.getStoreNameById);
@@ -24,14 +35,10 @@ router.get('/routes', routeController.getRoutes);
 router.get('/routes/nearby', routeController.getNearbyRoutes);
 router.get('/routes/:id', routeController.getRouteById);
 
-router.post('/visits', visitController.createVisit);
-router.get('/users/:userId/visits', visitController.getUserVisits);
-router.get('/users/:userId/visit-stats', visitController.getUserVisitStats);
-router.get('/stores/:storeId/visits', visitController.getStoreVisits);
-
-router.post('/route-starts', routeStartController.startRoute);
-router.get('/users/:userId/route-starts', routeStartController.getUserRouteStarts);
-router.get('/users/:userId/active-routes', routeStartController.getUserActiveRoutes);
-router.get('/routes/:routeId/starts', routeStartController.getRouteStarts);
+// Temporarily disable RouteStart routes causing the crash
+// router.post('/route-starts', routeStartController.startRoute);
+// router.get('/users/:userId/route-starts', routeStartController.getUserRouteStarts);
+// router.get('/users/:userId/active-routes', routeStartController.getUserActiveRoutes);
+// router.get('/routes/:routeId/starts', routeStartController.getRouteStarts);
 
 module.exports = router;
