7237ad45f8c163c9367b797f47e6c2bc5e9e986d
keerthan2102 <112113302+keerthan2102@users.noreply.github.com>
2025-11-01 10:17:15 -0400

    Update business.js

--- server/models/business.js
+++ server/models/business.js
@@ -3,20 +3,59 @@ const sequelize = require('../config/database');
 const bcrypt = require('bcryptjs');
 
 const Business = sequelize.define('Business', {
-    username: {
-        type: DataTypes.STRING,
-        unique: true,
-        allowNull: false,
+  id: { type: DataTypes.INTEGER, autoIncrement: true, primaryKey: true },
+
+  legalName: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+
+  email: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    unique: true,
+    validate: { isEmail: true },
+  },
+
+  phone: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+
+  address: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+
+  // Stored in DB
+  passwordHash: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+
+  // Not stored in DB, setting this will fill passwordHash
+  password: {
+    type: DataTypes.VIRTUAL,
+    set(value) {
+      this.setDataValue('password', value);
+      const salt = bcrypt.genSaltSync(10);
+      const hash = bcrypt.hashSync(value, salt);
+      this.setDataValue('passwordHash', hash);
     },
-    password: {
-        type: DataTypes.STRING,
-        allowNull: false,
-    },
+    validate: { len: [6, 72] },
+  },
+
+  status: {
+    type: DataTypes.ENUM('pending', 'approved'),
+    allowNull: false,
+    defaultValue: 'pending',
+  },
+}, {
+  tableName: 'businesses',
 });
 
-Business.beforeCreate(async (business) => {
-    const salt = await bcrypt.genSalt(10);
-    business.password = await bcrypt.hash(business.password, salt);
-});
+Business.prototype.checkPassword = function (plain) {
+  return bcrypt.compare(plain, this.passwordHash);
+};
 
 module.exports = Business;
