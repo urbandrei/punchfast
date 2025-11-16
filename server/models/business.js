60f348a83d0de26745cb26489fdc2384f2bd934b
Keerthan Challa <112113302+keerthan2102@users.noreply.github.com>
2025-11-15 22:02:54 -0500

    Update business.js

--- server/models/business.js
+++ server/models/business.js
@@ -7,13 +7,35 @@ const Business = sequelize.define('Business', {
         type: DataTypes.STRING,
         unique: true,
         allowNull: false,
+        set(v) {
+            this.setDataValue('username', String(v || '').trim().toLowerCase());
+        }
     },
     password: {
         type: DataTypes.STRING,
         allowNull: false,
     },
+
+    status: {
+        type: DataTypes.ENUM('pending', 'approved'),
+        allowNull: false,
+        defaultValue: 'pending',
+    },
+
+    goal: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        defaultValue: 10,
+    },
+
+    rewardText: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        defaultValue: 'Reward on completion',
+    }
 });
 
+// hash password automatically
 Business.beforeCreate(async (business) => {
     const salt = await bcrypt.genSalt(10);
     business.password = await bcrypt.hash(business.password, salt);
