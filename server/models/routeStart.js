c6567251218fefdeef4ad9b2ef65f058518e46e3
Aenjam <abhiramreddyenjam@gmail.com>
2025-11-16 13:54:13 -0500

    Add auth middleware import to routeStart model

--- server/models/routeStart.js
+++ server/models/routeStart.js
@@ -1,5 +1,6 @@
 const { DataTypes } = require('sequelize');
 const sequelize = require('../config/database');
+const auth = require("../middleware/auth");
 
 const RouteStart = sequelize.define('RouteStart', {
     userId: {
