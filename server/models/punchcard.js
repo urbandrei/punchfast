d6ccf3381b8f6d390f52f504e0eaf773048b0c41
Keerthan Challa <112113302+keerthan2102@users.noreply.github.com>
2025-11-15 22:03:16 -0500

    Update punchcard.js

--- server/models/punchcard.js
+++ server/models/punchcard.js
@@ -1,23 +1,33 @@
 const { DataTypes } = require('sequelize');
 const sequelize = require('../config/database');
 
-
 const Punchcard = sequelize.define('Punchcard', {
     customer_username: {
         type: DataTypes.STRING,
         allowNull: false,
+        set(v) {
+            this.setDataValue('customer_username', String(v || '').trim().toLowerCase());
+        }
     },
-   
     business_username: {
         type: DataTypes.STRING,
         allowNull: false,
+        set(v) {
+            this.setDataValue('business_username', String(v || '').trim().toLowerCase());
+        }
     },
     punches: {
         type: DataTypes.INTEGER,
-         allowNull: false,
+        allowNull: false,
+        defaultValue: 0,
     }
+}, {
+    indexes: [
+        {
+            unique: true,
+            fields: ['business_username', 'customer_username']
+        }
+    ]
 });
-
-
 
 module.exports = Punchcard;
