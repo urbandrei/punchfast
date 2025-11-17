1bc8bdfa6939f40a571520d7bafc926a54a2b703
Aenjam <abhiramreddyenjam@gmail.com>
2025-11-17 16:13:21 -0500

    Refactor data ingestion and address handling
    Refactored data ingestion process to remove duplicates and improve error handling.

--- server/data.js
+++ server/data.js
@@ -1,45 +1,67 @@
 const fs = require('fs');
 const Store = require('./models/store');
 const sequelize = require('./config/database');
-const { QueryTypes } = require('sequelize');
-
-
 async function dataIngestion(stores) {
-    for(i=0; i<stores.length; i++) {
-        name = stores[i].name;
-        address = stores[i].address;
-        latitude = stores[i].latitude;
-        longitude = stores[i].longitude;
-        const newStore = await Store.create({ name, address, latitude, longitude });
+    try {
+        console.log(`Starting ingestion of ${stores.length} stores...`);
+        const uniqueStores = [];
+        const seen = new Set();
+
+        for (const store of stores) {
+            if (!seen.has(store.name)) {
+                uniqueStores.push(store);
+                seen.add(store.name);
+            }
+        }
+
+        console.log(`After deduplication: ${uniqueStores.length} stores.`);
+        await Store.bulkCreate(uniqueStores, { ignoreDuplicates: true });
+
+        console.log("Data ingestion completed successfully.");
+    } catch (error) {
+        console.error("Error during data ingestion:", error);
     }
 }
 
-try {
-    const jsonData = fs.readFileSync('./stores.json', 'utf8');
-    const data = JSON.parse(jsonData);
-    stores = []
-    for(var i = 0; i < data.elements.length; i++) {
-        if(!data.elements[i].tags || !data.elements[i].tags.name || !data.elements[i].lat || !data.elements[i].lon) {
-            continue;
-        }
-        if(!data.elements[i].tags["addr:housenumber"] || !data.elements[i].tags["addr:street"] || !data.elements[i].tags["addr:city"] || !data.elements[i].tags["addr:state"] || !data.elements[i].tags["addr:postcode"]) {
-            store = {
-                name:data.elements[i].tags.name,
-                address:"unknown",
-                latitude:data.elements[i].lat,
-                longitude:data.elements[i].lon
+async function start() {
+    try {
+        await sequelize.authenticate();
+        console.log("Database connected.");
+
+        await sequelize.sync();
+        console.log("Database synced.");
+
+        const jsonData = fs.readFileSync('./stores.json', 'utf8');
+        const data = JSON.parse(jsonData);
+        const stores = [];
+        for (let i = 0; i < data.elements.length; i++) {
+            const elem = data.elements[i];
+            if (!elem.tags || !elem.tags.name || !elem.lat || !elem.lon) continue;
+            let address = "unknown";
+            if (
+                elem.tags["addr:housenumber"] &&
+                elem.tags["addr:street"] &&
+                elem.tags["addr:city"] &&
+                elem.tags["addr:state"] &&
+                elem.tags["addr:postcode"]
+            ) {
+                address =
+                    `${elem.tags["addr:housenumber"]} ` +
+                    `${elem.tags["addr:street"]}, ` +
+                    `${elem.tags["addr:city"]}, ` +
+                    `${elem.tags["addr:state"]} ` +
+                    `${elem.tags["addr:postcode"]}`;
             }
-        } else {
-            store = {
-                name:data.elements[i].tags.name,
-                address:data.elements[i].tags["addr:housenumber"] + " " + data.elements[i].tags["addr:street"] + ", " + data.elements[i].tags["addr:city"] + ", " + data.elements[i].tags["addr:state"] + " " + data.elements[i].tags["addr:postcode"],
-                latitude:data.elements[i].lat,
-                longitude:data.elements[i].lon
-            }
+            stores.push({
+                name: elem.tags.name,
+                address,
+                latitude: elem.lat,
+                longitude: elem.lon,
+            });
         }
-        stores.push(store);
+        await dataIngestion(stores);
+    } catch (error) {
+        console.error("Error reading/parsing JSON or DB error:", error);
     }
-    dataIngestion(stores);
-} catch (error) {
-  console.error('Error reading or parsing JSON file:', error);
 }
+start();
