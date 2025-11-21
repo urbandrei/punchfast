32bf665ea428b0b3a16b3791b357e5b8ce1e96b8
Aenjam <abhiramreddyenjam@gmail.com>
2025-11-21 05:13:39 -0500

    Update associations.js

--- server/models/associations.js
+++ server/models/associations.js
@@ -1,67 +1,4 @@
-const User = require('./user');
-const Business = require('./business');
-const Store = require('./store');
-const Route = require('./routes');
-const Punchcard = require('./punchcard');
-const Visit = require('./visit');
-const RouteStart = require('./routeStart');
-const RouteStore = require('./routeStore');
-const Search = require('./search');
-const SavedStore = require('./savedStore');
-
-User.hasMany(Visit, { foreignKey: 'userId', as: 'visits' });
-Visit.belongsTo(User, { foreignKey: 'userId', as: 'user' });
-
-Store.hasMany(Visit, { foreignKey: 'storeId', as: 'visits' });
-Visit.belongsTo(Store, { foreignKey: 'storeId', as: 'store' });
-
-User.hasMany(RouteStart, { foreignKey: 'userId', as: 'routeStarts' });
-RouteStart.belongsTo(User, { foreignKey: 'userId', as: 'user' });
-
-Route.hasMany(RouteStart, { foreignKey: 'routeId', as: 'routeStarts' });
-RouteStart.belongsTo(Route, { foreignKey: 'routeId', as: 'route' });
-
-Route.belongsToMany(Store, {
-    through: RouteStore,
-    foreignKey: 'routeId',
-    otherKey: 'storeId',
-    as: 'stores'
-});
-
-Store.belongsToMany(Route, {
-    through: RouteStore,
-    foreignKey: 'storeId',
-    otherKey: 'routeId',
-    as: 'routes'
-});
-
-Route.hasMany(RouteStore, { foreignKey: 'routeId', as: 'routeStores' });
-RouteStore.belongsTo(Route, { foreignKey: 'routeId', as: 'route' });
-
-Store.hasMany(RouteStore, { foreignKey: 'storeId', as: 'routeStores' });
-RouteStore.belongsTo(Store, { foreignKey: 'storeId', as: 'store' });
-
-User.hasMany(SavedStore, { foreignKey: 'userId', as: 'savedStores' });
-SavedStore.belongsTo(User, { foreignKey: 'userId', as: 'user' });
-
-Store.hasMany(SavedStore, { foreignKey: 'storeId', as: 'savedByUsers' });
-SavedStore.belongsTo(Store, { foreignKey: 'storeId', as: 'store' });
-
-User.belongsToMany(Store, {
-    through: SavedStore,
-    foreignKey: 'userId',
-    otherKey: 'storeId',
-    as: 'savedStoresList'
-});
-
-Store.belongsToMany(User, {
-    through: SavedStore,
-    foreignKey: 'storeId',
-    otherKey: 'userId',
-    as: 'savedByUsersList'
-});
-
-module.exports = {
+const {
     User,
     Business,
     Store,
@@ -72,6 +9,34 @@ module.exports = {
     RouteStore,
     Search,
     SavedStore
-};
+} = require('./index');
 
+User.hasMany(Visit, { foreignKey: 'userId' });
+Visit.belongsTo(User, { foreignKey: 'userId' });
 
+Store.hasMany(Visit, { foreignKey: 'storeId' });
+Visit.belongsTo(Store, { foreignKey: 'storeId' });
+
+User.hasMany(RouteStart, { foreignKey: 'userId' });
+RouteStart.belongsTo(User, { foreignKey: 'userId' });
+
+Route.hasMany(RouteStart, { foreignKey: 'routeId' });
+RouteStart.belongsTo(Route, { foreignKey: 'routeId' });
+
+Route.belongsToMany(Store, { through: RouteStore, foreignKey: 'routeId', otherKey: 'storeId' });
+Store.belongsToMany(Route, { through: RouteStore, foreignKey: 'storeId', otherKey: 'routeId' });
+
+Route.hasMany(RouteStore, { foreignKey: 'routeId' });
+RouteStore.belongsTo(Route, { foreignKey: 'routeId' });
+
+Store.hasMany(RouteStore, { foreignKey: 'storeId' });
+RouteStore.belongsTo(Store, { foreignKey: 'storeId' });
+
+User.hasMany(SavedStore, { foreignKey: 'userId' });
+SavedStore.belongsTo(User, { foreignKey: 'userId' });
+
+Store.hasMany(SavedStore, { foreignKey: 'storeId' });
+SavedStore.belongsTo(Store, { foreignKey: 'storeId' });
+
+User.belongsToMany(Store, { through: SavedStore, foreignKey: 'userId', otherKey: 'storeId' });
+Store.belongsToMany(User, { through: SavedStore, foreignKey: 'storeId', otherKey: 'userId' });
