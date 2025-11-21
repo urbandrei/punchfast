7a9e3b8c937a3cf4ed1488a8456695f9019963d4
Aenjam <abhiramreddyenjam@gmail.com>
2025-11-21 03:22:36 -0500

    Update user.js

--- server/models/user.js
+++ server/models/user.js
@@ -1,39 +1,30 @@
-const { DataTypes } = require("sequelize");
-const sequelize = require("../config/database");
-const bcrypt = require("bcryptjs");
+const { DataTypes } = require('sequelize');
+const sequelize = require('../config/database');
+const bcrypt = require('bcryptjs');
 
-const User = sequelize.define(
-  "User",
-  {
+const User = sequelize.define('User', {
     username: {
-      type: DataTypes.STRING,
-      unique: true,
-      allowNull: false,
+        type: DataTypes.STRING,
+        unique: true,
+        allowNull: false
     },
     email: {
-      type: DataTypes.STRING,
-      unique: true,
-      allowNull: false,
+        type: DataTypes.STRING,
+        unique: true,
+        allowNull: false
     },
     password: {
-      type: DataTypes.STRING,
-      allowNull: false,
+        type: DataTypes.STRING,
+        allowNull: false
     }
-  },
-  {
-    tableName: "users",
-  }
-);
-
-User.beforeCreate(async (u) => {
-  u.password = await bcrypt.hash(u.password, 10);
-});
-
-User.beforeUpdate(async (u) => {
-  if (u.changed("password")) {
-    u.password = await bcrypt.hash(u.password, 10);
-  }
+}, {
+    hooks: {
+        beforeCreate: async (user) => {
+            user.password = await bcrypt.hash(user.password, 10);
+        }
+    }
 });
 
 module.exports = User;
+
 
