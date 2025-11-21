036b785ce5f8639aa36713dda5ed6a111bdb03c3
Aenjam <abhiramreddyenjam@gmail.com>
2025-11-21 03:32:52 -0500

    Update store.js

--- server/models/store.js
+++ server/models/store.js
@@ -1,24 +1,17 @@
-const { DataTypes } = require('sequelize');
-const sequelize = require('../config/database');
+const { DataTypes } = require("sequelize");
+const sequelize = require("../config/database");
 
-const Store = sequelize.define('Store', {
-    name: {
-        type: DataTypes.STRING,
-        allowNull: false,
-    },
-    address: {
-        type: DataTypes.STRING,
-        allowNull: false,
-    },
-    latitude: {
-        type: DataTypes.FLOAT,
-        allowNull: false,
-    },
-    longitude: {
-        type: DataTypes.FLOAT,
-        allowNull: false,
-    },
+const Store = sequelize.define("Store", {
+    name: { type: DataTypes.STRING, allowNull: false },
+    address: { type: DataTypes.STRING, allowNull: false },
+    city: { type: DataTypes.STRING, allowNull: false },
+    state: { type: DataTypes.STRING, allowNull: false },
+    zipcode: { type: DataTypes.STRING, allowNull: false },
+    phone: { type: DataTypes.STRING },
+    website: { type: DataTypes.STRING },
+    description: { type: DataTypes.TEXT },
+    latitude: { type: DataTypes.FLOAT },
+    longitude: { type: DataTypes.FLOAT }
 });
-
 
 module.exports = Store;
