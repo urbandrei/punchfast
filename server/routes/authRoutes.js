6120cd025b1b9774145dc595eb70e1d25c147aaf
Aenjam <abhiramreddyenjam@gmail.com>
2025-11-21 03:19:25 -0500

    Update authRoutes.js

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -1,18 +1,16 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router();
-const authController = require("../controllers/authController");
+const authController = require('../controllers/authController');
 
-router.post("/signup", authController.signup);
-router.post("/business-signup", authController.businessSignup);
-
-router.post("/login/request-otp", authController.requestLoginOtp);
-router.post("/login/verify-otp", authController.verifyLoginOtp);
-
-router.post("/forgot-password", authController.forgotPassword);
-router.post("/verify-reset-otp", authController.verifyResetOtp);
-router.post("/reset-password", authController.resetPassword);
-
-router.post("/logout", authController.logout);
+router.post('/signup', authController.signup);
+router.post('/business-signup', authController.businessSignup);
+router.post('/login/request-otp', authController.requestLoginOtp);
+router.post('/login/verify-otp', authController.verifyLoginOtp);
+router.post('/forgot-password', authController.forgotPassword);
+router.post('/reset/verify-otp', authController.verifyResetOtp);
+router.post('/reset-password', authController.resetPassword);
+router.post('/logout', authController.logout);
 
 module.exports = router;
+
 
