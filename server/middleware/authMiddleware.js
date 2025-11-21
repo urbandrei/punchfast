3ff1c06eef38a069b7dd5337a883d0f065f7045d
Aenjam <abhiramreddyenjam@gmail.com>
2025-11-21 05:06:49 -0500

    Update authMiddleware.js

--- server/middleware/authMiddleware.js
+++ server/middleware/authMiddleware.js
@@ -1,17 +1,15 @@
-const jwt = require("jsonwebtoken");
+const jwt = require('jsonwebtoken');
 
 module.exports = (req, res, next) => {
     const token = req.cookies.token;
-
-    if (!token) {
-        return res.status(401).json({ message: "Unauthorized" });
-    }
+    if (!token) return res.status(401).json({ message: "Not authenticated" });
 
     try {
         const decoded = jwt.verify(token, process.env.JWT_SECRET);
         req.user = decoded;
         next();
-    } catch (err) {
-        return res.status(401).json({ message: "Invalid token" });
+    } catch {
+        res.status(401).json({ message: "Invalid token" });
     }
 };
+
