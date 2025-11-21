8745786b5d8aca7aa7f3935b540fe9d3eba7767b
Aenjam <abhiramreddyenjam@gmail.com>
2025-11-21 03:26:49 -0500

    Update authRoutes.js

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -1,19 +1,21 @@
-
-
 const express = require('express');
 const router = express.Router();
 const authController = require('../controllers/authController');
 
-router.post('/signup', authController.signup);
-router.post('/business-signup', authController.businessSignup);
 router.post('/login/request-otp', authController.requestLoginOtp);
 router.post('/login/verify-otp', authController.verifyLoginOtp);
-router.post('/forgot-password', authController.forgotPassword);
-router.post('/reset/verify-otp', authController.verifyResetOtp);
-router.post('/reset-password', authController.resetPassword);
+
+router.post('/signup', authController.signup);
+router.post('/signup/business', authController.businessSignup);
+
+router.post('/password/forgot', authController.forgotPassword);
+router.post('/password/verify-otp', authController.verifyResetOtp);
+router.post('/password/reset', authController.resetPassword);
+
 router.post('/logout', authController.logout);
 
 module.exports = router;
+
 
 
 
