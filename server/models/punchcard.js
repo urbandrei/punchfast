ca5db1d032c777b71f9f639bd61604beedcaf205
Aenjam <abhiramreddyenjam@gmail.com>
2025-11-21 03:34:27 -0500

    Update punchcard.js

--- server/models/punchcard.js
+++ server/models/punchcard.js
@@ -1,32 +1,9 @@
-const { DataTypes } = require('sequelize');
-const sequelize = require('../config/database');
+const { DataTypes } = require("sequelize");
+const sequelize = require("../config/database");
 
-const Punchcard = sequelize.define('Punchcard', {
-    customer_username: {
-        type: DataTypes.STRING,
-        allowNull: false,
-    },
-
-    business_username: {
-        type: DataTypes.STRING,
-        allowNull: false,
-    },
-
-    punches: {
-        type: DataTypes.INTEGER,
-        allowNull: false,
-        defaultValue: 0
-    }
-}, {
-    hooks: {
-        beforeValidate: (card) => {
-            if (card.customer_username)
-                card.customer_username = card.customer_username.toLowerCase().trim();
-
-            if (card.business_username)
-                card.business_username = card.business_username.toLowerCase().trim();
-        }
-    }
+const Punchcard = sequelize.define("Punchcard", {
+    punchesRequired: { type: DataTypes.INTEGER, allowNull: false },
+    reward: { type: DataTypes.STRING, allowNull: false }
 });
 
 module.exports = Punchcard;
