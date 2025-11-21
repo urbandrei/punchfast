d15a0aa6265017555b5caf644c132a55fb4f576f
Aenjam <abhiramreddyenjam@gmail.com>
2025-11-21 03:26:29 -0500

    Update database.js

--- server/config/database.js
+++ server/config/database.js
@@ -1,14 +1,15 @@
-const { Sequelize } = require("sequelize");
+const { Sequelize } = require('sequelize');
 
 const sequelize = new Sequelize(
-    process.env.POSTGRES_DB,
-    process.env.POSTGRES_USER,
-    process.env.POSTGRES_PASSWORD,
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD,
     {
         host: process.env.DB_HOST,
-        dialect: "postgres",
+        dialect: 'postgres',
         logging: false
     }
 );
 
 module.exports = sequelize;
+
