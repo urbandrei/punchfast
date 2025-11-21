9df009af586ce63f16ce96030a35fc2e1075311b
Aenjam <abhiramreddyenjam@gmail.com>
2025-11-21 03:22:54 -0500

    Update business.js

--- server/models/business.js
+++ server/models/business.js
@@ -1,38 +1,29 @@
-const { DataTypes } = require("sequelize");
-const sequelize = require("../config/database");
-const bcrypt = require("bcryptjs");
+const { DataTypes } = require('sequelize');
+const sequelize = require('../config/database');
+const bcrypt = require('bcryptjs');
 
-const Business = sequelize.define(
-  "Business",
-  {
+const Business = sequelize.define('Business', {
     username: {
-      type: DataTypes.STRING,
-      unique: true,
-      allowNull: false,
+        type: DataTypes.STRING,
+        unique: true,
+        allowNull: false
     },
     email: {
-      type: DataTypes.STRING,
-      unique: true,
-      allowNull: false,
+        type: DataTypes.STRING,
+        unique: true,
+        allowNull: false
     },
     password: {
-      type: DataTypes.STRING,
-      allowNull: false,
+        type: DataTypes.STRING,
+        allowNull: false
     }
-  },
-  {
-    tableName: "businesses",
-  }
-);
-
-Business.beforeCreate(async (b) => {
-  b.password = await bcrypt.hash(b.password, 10);
-});
-
-Business.beforeUpdate(async (b) => {
-  if (b.changed("password")) {
-    b.password = await bcrypt.hash(b.password, 10);
-  }
+}, {
+    hooks: {
+        beforeCreate: async (business) => {
+            business.password = await bcrypt.hash(business.password, 10);
+        }
+    }
 });
 
 module.exports = Business;
+
