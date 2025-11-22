5b503f5711e8d006727b8951807b9e07a98a37b2
Aenjam <abhiramreddyenjam@gmail.com>
2025-11-21 20:26:53 -0500

    Create index.js

---
+++ server/models/index.js
@@ -0,0 +1,23 @@
+const User = require('./user');
+const Business = require('./business');
+const Store = require('./store');
+const Route = require('./routes');
+const Punchcard = require('./punchcard');
+const Visit = require('./visit');
+const RouteStart = require('./routeStart');
+const RouteStore = require('./routeStore');
+const Search = require('./search');
+const SavedStore = require('./savedStore');
+
+module.exports = {
+    User,
+    Business,
+    Store,
+    Route,
+    Punchcard,
+    Visit,
+    RouteStart,
+    RouteStore,
+    Search,
+    SavedStore
+};
