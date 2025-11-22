e2e55df83c1c6238a121a77a8f362eaa8b887142
Aenjam <abhiramreddyenjam@gmail.com>
2025-11-21 19:13:55 -0500

    Update AuthModal.jsx

--- client/src/components/AuthModal.jsx
+++ client/src/components/AuthModal.jsx
@@ -1,7 +1,6 @@
 import React, { useState } from "react";
 import axios from "axios";
-export default function AuthModal() {
-  return null;
+
 }
 
 export default function UserAuthModal({ show, onClose, onLoginSuccess }) {
