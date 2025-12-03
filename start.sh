830c59989645938f73452c55f12305ffb80cf984
Andrei Roman <43176471+urbandrei@users.noreply.github.com>
2025-12-03 02:22:18 +0000

    adjusted start script

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 npm install
 (cd client && npm install && npm run build)
-(cd server && npm install)
+(cd server && npm install --ignore-scripts)
 NODE_ENV=production node server/index.js
